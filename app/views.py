aae3f6ad908dcbad99dfb2d883afefe31f39cf3f
antellgc <antellgc@gmail.com>
2017-08-17 22:34:55 -0400

    fixed dependencies

--- app/views.py
+++ app/views.py
@@ -9,21 +9,8 @@ database_path = './airbnb.db'
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Greg'}  # fake user
-    posts = [  # fake array of posts
-	    { 
-	        'author': {'nickname': 'John'}, 
-	        'body': 'Beautiful day in Portland!' 
-	    },
-	    { 
-	        'author': {'nickname': 'Susan'}, 
-	        'body': 'The Avengers movie was so cool!' 
-	    }
-	]
     return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+                           title='Home')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():

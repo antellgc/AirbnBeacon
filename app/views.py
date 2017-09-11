d0bc0a2a2f1e5b61dd4ff063afed12e760585ab0
antellgc <antellgc@gmail.com>
2017-09-10 20:04:35 -0400

    reorganization

--- app/views.py
+++ app/views.py
@@ -14,17 +14,6 @@ def index():
     return render_template("index.html",
                            title='Home')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for OpenID="%s", remember_me=%s' %
-			(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html',
-		title='Sign In',
-		form=form)
-
 @app.route('/db')
 def get_db():
 	conn = sqlite3.connect(database_path)
